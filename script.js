8e55c9285f55154777a1fdc55b85c3070c8cbafa
beewertheimer <beewertheimer@gmail.com>
2026-06-23 20:54:40 -0400

    redesign

--- script.js
+++ script.js
@@ -1,83 +1,19 @@
-filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
+let fontselectionHTML = '<link rel="preconnect" href="https://fonts.googleapis.com">' +
+'<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
+'<link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=IM+Fell+Great+Primer:ital@0;1&family=Jacquard+12&display=swap" rel="stylesheet"></link>';
+
+if (document.getElementById("fontselection")) {
+  document.getElementById("fontselection").innerHTML = fontselectionHTML;
 }
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+// let navHTML =  
 
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-let navHTML =  '<div class="nav-container">' +
-      '<div class="nav-container-item">' +
-        '<h1>|| bee wertheimer</h1>' +
-        '</div>' +
-          '<div class="nav-btn">' +
-          '<a href="https://beewertheimer.com">PUBLICATIONS</a>' +
-          '</div>' +
-          '<div class="nav-btn">' +
-          '<a href="https://beewertheimer.com/blog">BLOG</a>' +
-          '</div>' +
-          '<div class="nav-btn">' +
-          '<a href="https://beewertheimer.com/games">GAMES</a>' +
-          '</div>' +
-          '<div class="nav-btn">' +
-          '<a href="https://beewertheimer.com/art">ART</a>' +
-          '</div>' +
-          '<div class="nav-btn">' +
-          '<a href="https://beewertheimer.com/about">ABOUT</a>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-      '</div>';
-
-  if (document.getElementById("nav")) {
+if (document.getElementById("nav")) {
   document.getElementById("nav").innerHTML = navHTML;
 }
 
-let footerHTML =   '<div class="footer-container">' +
-      '<a href="mailto:beewertheimer@gmail.com" target="_blank">' + 
-      '<img src="images/email.png"/>' + '</a>' +
-      '<a href="https://beewertheimer.com/resume.pdf">' + 
-      '<img src="images/cv.svg">' + '</a>' +
-      '<a href="https://www.linkedin.com/in/beewertheimer/" target="_blank">' + 
-      '<img src="images/linkedin.png">' + 
-      '</a>' +
-      '<a href="https://bombylls.itch.io/" target="_blank">' + 
-      '<img src="images/itchio.svg">' + '</a>' +
-      '<a href="https://bsky.app/profile/beewertheimer.com" target="_blank">' + 
-      '<img src="images/bluesky.png">' + '</a>' +
-      '<p>this site was handmade using html, css, and javascript.' + '<br>' + 'thank you for visiting!</p>' +
-    '</div>';
-  
-  if (document.getElementById("footer")) {
+//let footerHTML =
+
+if (document.getElementById("footer")) {
   document.getElementById("footer").innerHTML = footerHTML;
 }
-
