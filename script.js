9d1951bb1592b9cc0bc7e7258d396e0c4a708026
beewertheimer <beewertheimer@gmail.com>
2026-06-25 12:39:01 -0400

    added filter archive

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-let fontselectionHTML = '<link rel="preconnect" href="https://fonts.googleapis.com">' +
-'<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
-'<link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=IM+Fell+Great+Primer:ital@0;1&family=Jacquard+12&display=swap" rel="stylesheet"></link>';
+filterSelection("all"); // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+let fontselectionHTML =
+  '<link rel="preconnect" href="https://fonts.googleapis.com">' +
+  '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
+  '<link href="https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer+SC&family=IM+Fell+Great+Primer:ital@0;1&family=Jacquard+12&display=swap" rel="stylesheet"></link>';
 
 if (document.getElementById("fontselection")) {
   document.getElementById("fontselection").innerHTML = fontselectionHTML;
 }
 
-// let navHTML =  
+// let navHTML =
 
 if (document.getElementById("nav")) {
   document.getElementById("nav").innerHTML = navHTML;
